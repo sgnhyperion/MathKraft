efb4c92db143d9a1e94059a30cfbf080830c8cfd
Harsh <hk755063@gmail.com>
2025-01-25 02:32:37 +0530

    page-4

--- src/app/(pages)/page4/page.tsx
+++ src/app/(pages)/page4/page.tsx
@@ -5,6 +5,10 @@ import HeaderLayout from "../../game/components/HeaderLayout";
 import { initialGameState } from "@/app/game/game-state";
 import Circle from "@/app/game/components/Circle";
 import { useSliceStore } from "@/store/useSliceStore";
+import { Card, CardHeader, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input";
+import { Select } from "@/components/ui/select";
+import Fraction from "@/app/game/components/Fraction";
 
 const page = () => {
   const { slice, setSlice } = useSliceStore();
@@ -21,7 +25,6 @@ const page = () => {
           variant="addition"
         />
 
-        {/* Middle Section */}
         {/* Middle Section */}
         <div className="flex justify-center items-center space-x-6 h-[25%] border-t-2 border-b-2 border-[#c0bdbd] w-full">
         <Circle 
@@ -44,7 +47,37 @@ const page = () => {
           circleColor="#d3ea00"   // Background color of the circle (e.g., light beige)
         />
         </div>
-        <Button text="Step 3" symbol=">>" />
+
+        
+        {/* Bottom Section */}
+        <div className="flex flex-col items-center pb-10 gap-4  w-full bg-[#fff6f6]">
+          
+          <div className="flex items-center justify-evenly w-[60%]">
+            <div className="text-[1.5vw]">So there are</div>
+            <Input />
+            <Fraction color="black" size="text-[1.5vw]" num="1" denom={4} />
+            <div className="text-[1.5vw]">sized pieces in 3 wholes</div>
+          </div>
+
+
+          <Card className="w-full max-w-[55%]">
+            <CardHeader className="bg-[#cbec9e] py-2">
+              <h2 className="text-center text-[1.8vw] uppercase">Awesome</h2>
+            </CardHeader>
+            <CardContent className="bg-[#f6f4c5] p-6">
+              <div className="flex items-center justify-center gap-6 text-[1.8vw]  h-[15vh]">
+                <span>{initialGameState.state1.mixedFraction.whole} wholes = </span>
+                <div className="flex flex-col items-center justify-evenly h-full">
+                  <Input />
+                  <div className="w-[3vw] bg-black h-[0.2rem]"></div>
+                  <div className=" text-[2vw] w-full flex justify-center">4</div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Button text="Step 3" symbol=">>" />
+        </div>
 
       </div>
     </div>
--- src/app/game/components/Fraction.tsx
+++ src/app/game/components/Fraction.tsx
@@ -4,7 +4,7 @@ const Fraction = ({color, size, num, denom=null}) => {
   return (
     <div className=" flex flex-col items-center h-full justify-center">
         <div className={`${size} ${color} font-bold `}>{num}</div>
-        {denom && <div className="border-t-2 border-[#a54dd7] w-6"></div>}
+        {denom && <div className="border-2 border-black w-6"></div>}
         {denom && <div className={`${size} ${color} font-bold `}>{denom}</div>}
     </div>
   )
